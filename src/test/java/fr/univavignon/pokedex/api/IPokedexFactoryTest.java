dd77f513c3a0518b346fe978f2811502e38c6cef
pierreAroban <pierre.aroban@gmail.com>
2017-03-29 16:18:10 +0200

    premier test

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -1,9 +1,13 @@
 package fr.univavignon.pokedex.api;
 
 import org.junit.*;
+
 import static org.junit.Assert.*;
+
 import org.mockito.*;
+
 import static org.mockito.Mockito.*;
+
 import org.mockito.junit.*;
 
 /**
@@ -13,25 +17,35 @@ public class IPokedexFactoryTest {
     @Rule
     public MockitoRule mockitoRule = MockitoJUnit.rule();
 
+    private static Pokemon pokemonMock = new Pokemon(
+            0,
+            "Bulbizarre",
+            126,
+            126,
+            90,
+            613,
+            64,
+            4000,
+            4,
+            56
+    );
+
     @Mock
-    private Pokemon pokemonMock;
+    private static IPokemonFactory pokemonFactoryMock;
 
     @Before
     public void setUp() {
-        when(pokemonMock.getName()).thenReturn("Bulbizarre");
-        when(pokemonMock.getName()).thenThrow(new Exception());
+        MockitoAnnotations.initMocks(this);
+        when(pokemonFactoryMock.createPokemon(0, 613, 64, 4000, 4)).thenReturn(pokemonMock);
     }
 
     /**
      * Creates a new pokedex instance using the given
      * <tt>metadataProvider</tt> and <tt>pokemonFactory</tt>.
-     *
-     * @param metadataProvider Metadata provider the created pokedex will use.
-     * @param pokemonFactory   Pokemon factory the created pokedex will use.
-     * @return Created pokedex instance.
      */
     @Test
-    void testCreatePokedex() {
-        assertEquals("Bulbizarre", this.pokemonMock.getName());
+    public void testCreatePokedex() {
+        Pokemon pokemon = pokemonFactoryMock.createPokemon(0, 613, 64, 4000, 4);
+        assertEquals("Bulbizarre", pokemon.getName());
     }
 }
